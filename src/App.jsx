56e9aed3f5aac452e8061f2c764876253b21f544
Alberto Wang <Alberto.Wang1995@gmail.com>
2025-12-09 23:48:44 -0300

    Clon de AWARDS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500 font-bold text-[#FF6A3D]">Welcome to AWARDS </h1>
+    </main>
+  )
+}
+
+export default App
